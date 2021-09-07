ee64c4cbe0309b59419b6bbefb4f0b941cf4bced
Dong-Hun <1031nice@gmail.com>
2021-09-07 23:38:22 +0900

    프로젝트 생성 및 의존성 추가

---
+++ src/main/java/me/donghun/eventrestapiserver/EventRestApiServerApplication.java
@@ -0,0 +1,13 @@
+package me.donghun.eventrestapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRestApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRestApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/donghun/eventrestapiserver/EventRestApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.donghun.eventrestapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRestApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
