8056ad19ee64add381a54cf4e55d55b670f06a9c
Dong-Hun <1031nice@gmail.com>
2021-09-09 20:24:48 +0900

    [추가] Event 생성 테스트

---
+++ src/test/java/me/donghun/eventrestapiserver/events/EventControllerTest.java
@@ -0,0 +1,29 @@
+package me.donghun.eventrestapiserver.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+}
