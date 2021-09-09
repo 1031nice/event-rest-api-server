b465254c9acb80ae18a84720c6e04b3e86a18403
Dong-Hun <1031nice@gmail.com>
2021-09-09 20:41:42 +0900

    [추가] Event 생성 API 구현(DB 미사용) 및 테스트 보완

---
+++ src/main/java/me/donghun/eventrestapiserver/events/EventController.java
@@ -0,0 +1,27 @@
+package me.donghun.eventrestapiserver.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        // TODO DB에 들어갈 때 값이 자동으로 설정되도록
+        event.setId(1);
+        return ResponseEntity.created(createdUri).body(event);
+    }
+
+}
--- src/test/java/me/donghun/eventrestapiserver/events/EventControllerTest.java
+++ src/test/java/me/donghun/eventrestapiserver/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package me.donghun.eventrestapiserver.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,8 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -17,12 +21,30 @@ class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 9, 9, 20, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 9, 10, 20, 30))
+                .beginEventDateTime(LocalDateTime.of(2021, 9, 11, 20, 30))
+                .endEventDateTime(LocalDateTime.of(2021, 9, 12, 20, 30))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaTypes.HAL_JSON))
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
                 .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
                 .andDo(print());
     }
 
